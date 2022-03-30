487daa1f5e93c04331938a17da8a10a9f2b624c3
minimizer28 <Rey.MarcPlazo@gmail.com>
2022-03-30 12:58:01 +0800

    runtime url buildpack

--- config/runtime.exs
+++ config/runtime.exs
@@ -44,7 +44,7 @@ if config_env() == :prod do
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :portfolio, PortfolioWeb.Endpoint,
-    url: [host: host, port: 443],
+    url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
