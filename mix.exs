cc16730f6dabebe8d142209389e3d5a124ffd759
minimizer28 <Rey.MarcPlazo@gmail.com>
2022-03-30 13:47:12 +0800

    fixing assets deplyment

--- mix.exs
+++ mix.exs
@@ -60,12 +60,12 @@ defmodule Portfolio.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "cmd --cd assets npm install"],
+      setup: ["deps.get", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": [
-        "cmd --cd assets npm run deploy",
+        "cmd npm run deploy",
         "esbuild default --minify",
         "phx.digest"]
     ]
