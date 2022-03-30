23270fed39b6d36c2c87644b666ea1eb05eb7e50
minimizer28 <Rey.MarcPlazo@gmail.com>
2022-03-30 14:37:54 +0800

    fixing assets deplyment add new deply script for tailwind 3

--- config/dev.exs
+++ config/dev.exs
@@ -24,6 +24,8 @@ config :portfolio, PortfolioWeb.Endpoint,
   debug_errors: true,
   secret_key_base: "fI0Wf+JY2k5IrgYD285N/gWKHZZ5cYvNwW3EkXi+KWa++uI0QuV/OT8jsFU3bUEV",
   watchers: [
+    # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
     npx: [
       "tailwindcss",
       "--input=css/app.css",
@@ -31,9 +33,7 @@ config :portfolio, PortfolioWeb.Endpoint,
       "--postcss",
       "--watch",
       cd: Path.expand("../assets", __DIR__)
-    ],
-    # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    ]
   ]
 
 # ## SSL Support
--- mix.exs
+++ mix.exs
@@ -65,8 +65,6 @@ defmodule Portfolio.MixProject do
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
       "assets.deploy": [
-        "cmd --cd assets npm run deploy",
-        "cmd --cd assets npm run deploy-tailwind",
         "esbuild default --minify",
         "phx.digest"]
     ]
