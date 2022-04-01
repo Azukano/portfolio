093fe15008ae7f93231ad94c3ed89151f3a79293
minimizer28 <Rey.MarcPlazo@gmail.com>
2022-04-01 11:51:58 +0800

    header done

--- assets/tailwind.config.js
+++ assets/tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ["../lib/*_web/**/*.*ex", "./js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        'topbar': '#E6E6E6'
+      },
+      backgroundColor: {
+        'main': '#242526'
+      }
+    },
   },
   plugins: [],
 }
