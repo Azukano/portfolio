0861b3d8fbe26b0e6074c260e4e576599f94bee2
minimizer28 <Rey.MarcPlazo@gmail.com>
2022-04-21 15:28:30 +0800

    updated hiremecontroller with flash

--- lib/portfolio_web/controllers/hireme_controller.ex
+++ lib/portfolio_web/controllers/hireme_controller.ex
@@ -4,28 +4,31 @@ defmodule PortfolioWeb.HiremeController do
 
   use PortfolioWeb, :controller
 
-  def index(conn, _params) do
+  def index(conn, _) do
     changeset = Send.changeset(%Send{}, %{})
     render(conn, "index.html", changeset: changeset)
   end
 
   def create(conn, %{"send" => message_params}) do
+    IO.inspect message_params
     case Message.send_message(message_params) do
       {:ok, _} ->
         conn
         |> put_flash(:info, "Message Sent!")
         |> redirect(to: Routes.hireme_path(conn, :index))
       {:error, %Ecto.Changeset{} = changeset} ->
+        conn = put_flash(conn, :error, "Error Sending!")
         render(conn, "index.html", changeset: changeset)
-      # _ ->
-      #   conn
-      #   |> put_flash(:info, "Message Sent!")
-      #   |> redirect(to: Routes.hireme_path(conn, :index))
     end
   end
 
-  def show(conn, _) do
-    messages = Portfolio.Pagination.page("messages", 10, 10)
-    render(conn, "show.html", messages: messages)
+  def show(conn, %{"rec_perpage" => rec_perpage}) do
+    conn
+    |> assign("rec_perpage", 10)
+    messages = Portfolio.Pagination.page("messages", rec_perpage, 10)
+    render(conn, "show.html", messages: messages, rec_perpage: rec_perpage)
   end
+
 end
+
+#%{"rec_perpage" => rec_perpage}
