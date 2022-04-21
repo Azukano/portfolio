df2f1dfdd4af1722f5c7947e2509c704963d2358
minimizer28 <Rey.MarcPlazo@gmail.com>
2022-04-21 13:08:49 +0800

    updated query - max_id, + limit records by requested per page

--- lib/portfolio/pagination.ex
+++ lib/portfolio/pagination.ex
@@ -4,19 +4,16 @@ defmodule Portfolio.Pagination do
 
   def fetch_records(query, rec_perpage, page_no) do
     min_id = rec_perpage*(page_no - 1) + 1
-    max_id = rec_perpage*page_no
     query
       |> where([m], m.id >= ^min_id)
-      |> where([m], m.id <= ^max_id)
-      |> select([m], %{name: m.name, email: m.email, message: m.message})
+      |> limit(^rec_perpage)
+      |> order_by(desc: :id)
+      |> select([:id, :name, :email, :message])
       |> Repo.all()
   end
 
   def page(query, rec_perpage, page_no) do
+    IO.inspect(fetch_records(query, rec_perpage, page_no))
     fetch_records(query, rec_perpage, page_no)
   end
-
-  # def count_records do
-
-  # end
 end
